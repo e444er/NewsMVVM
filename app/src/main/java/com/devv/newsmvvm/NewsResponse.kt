be4674ae12f76e227e423a805eec7d513af1e2eb
e444er <e444er@@gmail.com>
2020-12-27 23:08:24 +0600

    retrofit

---
+++ app/src/main/java/com/devv/newsmvvm/NewsResponse.kt
@@ -0,0 +1,7 @@
+package com.devv.newsmvvm
+
+data class NewsResponse(
+    val articles: List<Article>,
+    val status: String,
+    val totalResults: Int
+)
---
+++ app/src/main/java/com/devv/newsmvvm/Source.kt
@@ -0,0 +1,6 @@
+package com.devv.newsmvvm
+
+data class Source(
+    val id: Any,
+    val name: String
+)
